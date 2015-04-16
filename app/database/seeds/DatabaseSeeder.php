b7b65edae6146cac1a38dafbe4e8ff0b4709ea6d
Lemonka93 <lerayepishko@gmail.com>
2015-04-16 21:31:57 +0300

    random_user_planet

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,6 +10,11 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+        $this->call('UserTableSeeder');
+        $this->command->info('User table seeder!');
+
+        $this->call('PlanetTableSeeder');
+        $this->command->info('Planet table seeder!');
 
 		// $this->call('UserTableSeeder');
 	}
---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Lemonka
+ * Date: 16.04.2015
+ * Time: 19:54
+ */
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $count = rand(1, 10);
+        for ($i = 0; $i < $count; $i++) {
+            $user = new User();
+            $user->email = 'user_' . $i . '@mysite.dev';
+            $user->password = Hash::make('12345678');
+            $user->username = 'user_' . $i;
+            $user->isAdmin = rand(0, 1);
+            $user->isActive = rand(0, 1);
+            $user->activationCode = '';
+            $user->remember_token = self::generateRandomsString(100);
+            $user->save();
+    }
+    }
+
+    /**
+     * Generate random string
+     *
+     * @param int $length
+     * @return string
+     */
+    private  function generateRandomsString($length = 10) {
+        $characters = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0;$i<$length;$i++){
+            $randomString .=$characters[rand(0, $charactersLength-1)];
+        }
+          return $randomString;
+    }
+}
